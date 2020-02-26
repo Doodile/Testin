1237ae2adc1326183325b4121f05ff891bfabd62
Nick <frankencookie@btinternet.com>
2020-02-26 22:47:43 +0000

    door update

--- Assets/Daniel/Door.cs
+++ Assets/Daniel/Door.cs
@@ -6,24 +6,51 @@ using UnityEngine;
 
 public class Door : BaseInteract_Nick
 {
-    public bool IsOpening=false;
-    public float startY;
-    public float endY;
+    public bool IsOpening = false;
+    private Vector3 InitialPos, OpenPos;
+    public float OpenRotation, DoorOpenSpeed;
+    private float x, y, z;
+    public bool Locked;
+
+    private void Start()
+    {
+        //Get the rotation values 
+        x = transform.eulerAngles.x;
+        y = transform.eulerAngles.y;
+        z = transform.eulerAngles.z;
+
+        //Set the initial values
+        InitialPos = new Vector3(x, y, z);
+
+        //Set the open values
+        OpenPos = new Vector3(x, y + OpenRotation, z);
+    }
 
     public override void Interact()
     {
-        IsOpening = !IsOpening;
+        if (Locked)
+        {
+            IsOpening = !IsOpening;
+        }
+
     }
 
     void FixedUpdate()
     {
         if (IsOpening)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-89.98f, endY, 0), 1.5f * Time.deltaTime);
+            //Open door
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(OpenPos), DoorOpenSpeed * Time.deltaTime);
         }
         else
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-89.98f, startY, 0), 1.5f * Time.deltaTime);
+            //Close door
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(InitialPos), DoorOpenSpeed * Time.deltaTime);
         }
+    }
+
+    public void LockPickDone()
+    {
+        Locked = false;
     }
 }
