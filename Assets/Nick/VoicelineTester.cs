1ee604ef55b35bb4c3746052ac7ca98c80e9b68e
Nick <frankencookie@btinternet.com>
2020-03-23 13:17:44 +0000

    Optimisation + Subtitles

---
+++ Assets/Nick/VoicelineTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoicelineTester : BaseInteract_Nick
+{
+    SFXManager voiceline;
+    public bool UseEnums = true;
+
+    public EVoiceClips clipToPlay;
+    public AudioClip audioClip;
+    public string Subs;
+
+
+    public override void Interact()
+    {
+        if(UseEnums)
+        {
+            SFXManager.GlobalSFXManager.PlayVoiceClip(clipToPlay);
+            //voiceline.PlayVoiceClip(clipToPlay);
+        }
+        else
+        {
+            SFXManager.GlobalSFXManager.PlayVoiceClip(audioClip, Subs);
+            //voiceline.PlayVoiceClip(audioClip, Subs);
+        }
+    }
+}
--- Assets/Wijnand/Audio/Script/SFXManager.cs
+++ Assets/Wijnand/Audio/Script/SFXManager.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
-
-
     public List<string> ClipNames = new List<string>();
     public List<AudioClip> ClipList = new List<AudioClip>();
+
+    //Nick
+    public string[] VoiceClipSubtitles = new string[(int)EVoiceClips.CLIP_LENGTH];
+    public AudioClip[] VoiceClipList = new AudioClip[(int)EVoiceClips.CLIP_LENGTH];
+
+    Text SubtitleText;
+    //Nick End
 
     public GameObject SFX_Prefab;
 
@@ -23,6 +29,10 @@ public class SFXManager : MonoBehaviour
         {
             SFX_Lib.Add(ClipNames[i], ClipList[i]);
         }
+        //Nick
+        SubtitleText = GameObject.Find("Subtitles").GetComponent<Text>();
+        SubtitleText.enabled = false;
+        //Nick End
     }
 
 
@@ -51,11 +61,64 @@ public class SFXManager : MonoBehaviour
         }
     }
 
+    //Nick
+    //Play voiceline and display subtitles for audio clip
+    public void PlayVoiceClip(EVoiceClips clip)
+    {
+        AudioSource SFX = Instantiate(SFX_Prefab).GetComponent<AudioSource>();
+        SFX.PlayOneShot(VoiceClipList[(int)clip]);
 
-  
+        SubtitleText.enabled = true;
+        SubtitleText.text = VoiceClipSubtitles[(int)clip];
+        StartCoroutine(SubtitleDelay(VoiceClipList[(int)clip].length));
 
+        Destroy(SFX.gameObject, VoiceClipList[(int)clip].length);
+    }
+    //Alternate version wihout enum
+    public void PlayVoiceClip(AudioClip clip, string sub)
+    {
+        AudioSource SFX = Instantiate(SFX_Prefab).GetComponent<AudioSource>();
+        SFX.PlayOneShot(clip);
 
+        SubtitleText.enabled = true;
+        SubtitleText.text = sub;
+        StartCoroutine(SubtitleDelay(clip.length));
 
+        Destroy(SFX.gameObject, clip.length);
+    }
 
+    IEnumerator SubtitleDelay(float delay)
+    {
+        //Debug.Log("Start");
+        yield return new WaitForSeconds(delay);
 
+        SubtitleText.enabled = false;
+    }
+
+}
+
+//Nick
+//All the voicelines.
+public enum EVoiceClips : int
+{
+    CLIP_TEST,
+    CLIP_DONTWANT,
+    //Alanna code added//
+    key_dropped, 
+    candle_pickup, 
+    candle_pickup_2, 
+    piece_of_clothing, 
+    basement_key, 
+    office_key_door, 
+    knife_pickup, 
+    finding_switch_generator, 
+    found_switch, 
+    basement_book, 
+    basement_secret_open, 
+    pencil_found, 
+    paper_found, 
+    start_ritual,
+    //Alanna End//
+
+    CLIP_LENGTH
 }
